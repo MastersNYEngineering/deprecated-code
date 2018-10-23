309dbc8cb4fcd9c8cf7a1cf2bf23cb66d3b73dc5
xoreo <mattnappo@gmail.com>
2018-10-23 17:11:11 -0400

    Put move into func

--- src/Triangle.java
+++ src/Triangle.java
@@ -16,6 +16,44 @@ public class TriangleDrive extends OpMode {
     private DcMotor w1 = null;
     private DcMotor w2 = null;
 
+    void move() {
+        double x_left_joy = gamepad1.left_stick_x;
+        double y_left_joy = gamepad1.left_stick_y;
+        
+        double phi_joy = Math.atan2(y_left_joy, x_left_joy);
+        
+        double x_left_joy_sq = Math.pow(x_left_joy, 2);
+        double y_left_joy_sq = Math.pow(y_left_joy, 2);
+        
+        double r_joy = Math.sqrt(x_left_joy_sq + y_left_joy_sq);
+        
+        double max_speed = 1;
+        double speed = max_speed * r_joy;
+        
+        double alpha_1 = Math.PI / 2;
+        double alpha_2 = 7 * Math.PI / 6;
+        double alpha_3 = 11 * Math.PI / 6;
+        
+        double theta_1 = alpha_1 - phi_joy;
+        double theta_2 = alpha_2 - phi_joy;
+        double theta_3 = alpha_3 - phi_joy;
+        
+        double w0_power = speed * Math.sin(theta_1);
+        double w1_power = speed * Math.sin(theta_2);
+        double w2_power = speed * Math.sin(theta_3);
+        
+        telemetry.addData("w0_power", w0_power);
+        telemetry.addData("w1_power", w1_power);
+        telemetry.addData("w2_power", w2_power);
+        
+        w0.setPower(-w0_power);
+        w1.setPower(-w1_power);
+        w2.setPower(-w2_power);
+    }
+
+    
+
+        
     @Override
     public void init() {
         w0 = hardwareMap.get(DcMotor.class, "w0");
@@ -44,44 +82,16 @@ public class TriangleDrive extends OpMode {
 
     @Override
     public void loop() {
-        double x_joy = gamepad1.left_stick_x;
-        double y_joy = gamepad1.left_stick_y;
-
-        double phi_joy = Math.atan2(y_joy, x_joy);
-        
-        double x_joy_sq = Math.pow(x_joy, 2);
-        double y_joy_sq = Math.pow(y_joy, 2);
-        
-        double r_joy = Math.sqrt(x_joy_sq + y_joy_sq);
-
-        double max_speed = 1;
-        double speed = max_speed * r_joy;
-
-        double alpha_1 = Math.PI / 2;
-        double alpha_2 = 7 * Math.PI / 6;
-        double alpha_3 = 11 * Math.PI / 6;
-
-        double theta_1 = alpha_1 - phi_joy;
-        double theta_2 = alpha_2 - phi_joy;
-        double theta_3 = alpha_3 - phi_joy;
-
-        double w0_power = speed * Math.sin(theta_1);
-        double w1_power = speed * Math.sin(theta_2);
-        double w2_power = speed * Math.sin(theta_3);
-        
-        telemetry.addData("w0_power", w0_power);
-        telemetry.addData("w1_power", w1_power);
-        telemetry.addData("w2_power", w2_power);
-
-        w0.setPower(-w0_power);
-        w1.setPower(-w1_power);
-        w2.setPower(-w2_power);
+        move();
+        // turn();
 
         telemetry.addData("Run Time", runtime.toString());
     }
 
     @Override
     public void stop() {
-
+        w0 = null;
+        w1 = null;
+        w2 = null;
     }
 }
